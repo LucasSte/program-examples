00e034841881e653231e31d787957612c6f80382
Lucas <lucas.tnagel@gmail.com>
2024-03-18 19:16:17 -0300

    Saving good tests

--- basics/rent/native/program/src/lib.rs
+++ basics/rent/native/program/src/lib.rs
@@ -12,6 +12,13 @@ use solana_program::{
 
 
 entrypoint!(entry);
+
+struct Testing {
+    a: u128,
+    b: u128
+}
+
+// TODO: This test must be included in programs/sbf
 pub fn entry(program_id: &Pubkey, accounts: &[AccountInfo], data: &[u8])
              -> ProgramResult {
     
@@ -26,8 +33,9 @@ pub fn entry(program_id: &Pubkey, accounts: &[AccountInfo], data: &[u8])
 
     let buffer: [u8; 3916] = [1; 3916];
 
-    lol(x);
+    let y = lol(x);
 
+    assert_eq!(y[15], 17);
     msg!("{:p} (should start with 0x2000..)", &buffer);
     msg!("{:?} (should be 1)", buffer[0]);
     assert_eq!(buffer[0], 1);
@@ -36,19 +44,78 @@ pub fn entry(program_id: &Pubkey, accounts: &[AccountInfo], data: &[u8])
 
     msg!("{:?}", x);
 
-    // //msg!("b: {:?}", buffer);
-    for i in 0..3916 {
-        assert_eq!(buffer[i], 1);
-    }
+    let x_i128 : u128 = u128::MAX - 2;
+    let y_i128 = test_i128(x_i128);
+    assert_eq!(y_i128, u128::MAX);
+    
+
+    let x_256 = Testing {a: u128::MAX - 2, b: u128::MAX - 3};
+    let y_256 = modify_256(x_256);
+
+    let res = many_args(51, -89, 85, -777, 500, 35, 100, 45);
+    assert_eq!(res, -1420);
+    
+    let res = many_args_stack_space(51, -89, 85, -777, 500, 35, 100, 45);
+    assert_eq!(res, -1420);
+
     Ok(())
 }
 
 #[inline(never)]
-extern fn lol(x: [u8; 16]) {
+extern fn lol(mut x: [u8; 16]) -> [u8; 16] {
     for i in 0..1 {
         assert!(x[i] != 1 as u8);
     }
     msg!("{:?}", x);
     assert_eq!(x[11], 0x37);
     assert_eq!(x[10], 0x39);
+    x[15] = 17;
+    return x;
 }
+
+#[inline(never)]
+extern fn test_i128(x: u128) -> u128 {
+    let y = x + 2;
+    return y;
+}
+
+#[inline(never)]
+extern fn modify_256(x: Testing) -> Testing {
+    let y = Testing {
+        a: x.a + 2,
+        b: x.b + 3
+    };
+    return y;
+}
+
+#[inline(never)]
+// (51, -89, 85, -777, 500, 35, 100, 45);
+extern fn many_args(a: i64, b: i64, c: i64, d: i64, e: i64, f: i64, g: i64, h: i64) -> i64 {
+    let i = a + b; // 51 - 89 = -38
+    let j = i - c; // -38 -85 = -123
+    let k = j + d; // -123 - 777 = -900
+    let l = k - e; // -900 - 500 = -1400
+    let m = l + f; // -1400 + 35 = 
+    let n = m - g;
+    let o = n + h;
+    return o;
+}
+
+#[inline(never)]
+extern fn many_args_stack_space(a: i64, b: i64, c: i64, d: i64, e: i64, f: i64, g: i64, h: i64) -> i64 {
+    let mut s: [i64; 3] = [1, 2, 3]; 
+    let i = a + b; // 51 - 89 = -38
+    let j = i - c; // -38 -85 = -123
+    let k = j + d; // -123 - 777 = -900
+    let l = k - e; // -900 - 500 = -1400
+    let m = l + f; // -1400 + 35 = 
+    let n = m - g;
+    let o = n + h;
+    let p = o + s[0];
+    let q = p + s[1];
+    let r = q - s[2];
+    return r;
+}
+
+
+// Test more than 5 arguments, and argument whose size is greater than 128
