61dc9ec60f4ec1c0e4a3c56ec40fde4ee02ad6cd
Lucas Steuernagel <lucas.tnagel@gmail.com>
2024-05-24 12:04:12 -0300

    Test mod isntr

--- basics/rent/native/program/src/lib.rs
+++ basics/rent/native/program/src/lib.rs
@@ -1,49 +1,25 @@
 use solana_program::{
-    account_info::{next_account_info, AccountInfo},
+    account_info::AccountInfo,
     entrypoint,
     entrypoint::ProgramResult,
-    msg,
-    program::invoke,
+    program::set_return_data,
     pubkey::Pubkey,
-    rent::Rent,
-    system_instruction, system_program,
-    sysvar::Sysvar,
 };
 
 entrypoint!(process_instruction);
 
 fn process_instruction(
     _program_id: &Pubkey,
-    accounts: &[AccountInfo],
+    _accounts: &[AccountInfo],
     instruction_data: &[u8],
 ) -> ProgramResult {
-    let accounts_iter = &mut accounts.iter();
-    let payer = next_account_info(accounts_iter)?;
-    let new_account = next_account_info(accounts_iter)?;
-    let system_program = next_account_info(accounts_iter)?;
+    // TODO: Build with my custom toolchain!
+    let a = u64::from_be_bytes(instruction_data[0..8].try_into().unwrap());
+    let b = u64::from_be_bytes(instruction_data[8..16].try_into().unwrap());
 
-    msg!("Program invoked. Creating a system account...");
-    msg!("  New public key will be: {}", &new_account.key.to_string());
+    let res = a % b;
 
-    // Determine the necessary minimum rent by calculating the account's size
-    //
-    let account_span = instruction_data.len();
-    let lamports_required = (Rent::get()?).minimum_balance(account_span);
-
-    msg!("Account span: {}", &account_span);
-    msg!("Lamports required: {}", &lamports_required);
-
-    invoke(
-        &system_instruction::create_account(
-            payer.key,
-            new_account.key,
-            lamports_required,
-            account_span as u64,
-            &system_program::ID,
-        ),
-        &[payer.clone(), new_account.clone(), system_program.clone()],
-    )?;
-
-    msg!("Account created succesfully.");
+    let data = res.to_be_bytes();
+    set_return_data(&data);
     Ok(())
 }
